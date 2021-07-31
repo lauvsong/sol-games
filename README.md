6d0add326b8b44a6208614c000a7f147656c118a
lauvsong <hyehye9355@naver.com>
2021-08-01 03:05:32 +0900

    Implement rock-paper-scissors.sol

---
+++ README.md
@@ -0,0 +1,8 @@
+# sol-games
+simple games base on `solidity`
+
+## games
+1. rock-paper-scissors
+2. slot machine
+3. horse racing
+4. bulls and cows
---
+++ rock-paper-scissors.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.6.0;
+
+contract Game {
+    uint8 constant ROCK = 0;
+    uint8 constant PAPER = 1;
+    uint8 constant SCISSORS = 2;
+    
+    mapping(address => uint8) public choices;
+    
+    function play(uint8 choice) external {
+        // check the move is valid
+        require(choice == ROCK || choice == PAPER || choice == SCISSORS);
+        // check the player hasnt played the move
+        require(choices[msg.sender] == 0);
+        
+        //set choice
+        choices[msg.sender] = choice;
+    }
+    
+    function evaluate(address alice, address bob) external view returns (address) {
+        //  if draw
+        if (choices[alice] == choices[bob]) {
+            return address(0);
+        }
+        
+        // set winning case
+        if (choices[alice] == ROCK && choices[bob] == PAPER) {
+            return bob;
+        } else if (choices[alice] == ROCK && choices[bob] == SCISSORS) {
+            return alice;
+        } else if (choices[alice] == PAPER && choices[bob] == ROCK) {
+            return alice;
+        } else if (choices[alice] == PAPER && choices[bob] == SCISSORS) {
+            return bob;
+        } else if (choices[alice] == SCISSORS && choices[bob] == ROCK) {
+            return bob;
+        } else if (choices[alice] == SCISSORS && choices[bob] == PAPER) {
+            return alice;
+        }
+        
+    }
+}
