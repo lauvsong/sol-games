b25a3c55124c6899cecbe894a8cb6a0347fe639a
lauvsong <hyehye9355@naver.com>
2021-08-03 04:13:06 +0900

    Fix slot-machine error

--- slot-machine.sol
+++ slot-machine.sol
@@ -2,7 +2,13 @@ pragma solidity ^0.6.0;
 
 contract Game {
     
-    mapping(address => uint) public results;
+    struct Gamer {
+        bool jackpot;
+        bool isExist;
+    }
+    
+    mapping(address => Gamer) public gamers;
+    
     string[9] public items = ["CHERRY", "LEMON", "DIAMOND", "WATERMELON", "BELL", "HEART", "SEVEN", "BAR", "HORSE"];
     string[2] public outputs = ["JACKPOT" , "Try again"];
     
@@ -12,28 +18,28 @@ contract Game {
         uint256 userBalance = msg.value;
         require(userBalance > 0, "Balance too low!");
         
+        gamers[msg.sender].isExist = true;
+        
         uint first = spin();
         uint second = spin();
         uint third = spin();
         
         if (first == 7 && (first == second) && (second == third)){
-            results[msg.sender] = 1;
-        } else {
-            results[msg.sender] = 2;
+            gamers[msg.sender].jackpot = true;
         }
     }
     
     // spin wheel (get random value)
     function spin() private view returns (uint) {
-        return uint(keccak256(abi.encodePacked(block.timestamp))) % 10 + 1;
+        return uint(keccak256(abi.encodePacked(block.timestamp))) % 10;
     }
     
     // print result
     function getResult(address user) external returns (string memory) {
         
-        require(results[user] != 0, "User not exists");
+        require(gamers[user].isExist, "User not exists");
         
-        if (results[user] == 1) return outputs[0];
+        if (gamers[user].jackpot) return outputs[0];
         else return outputs[1];
     }
 }
